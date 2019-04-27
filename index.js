e8b5c8a6359d703670557e105917cf2ac3c3a71d
Changpeng Duan <838560574@qq.com>
2019-04-27 15:58:01 +0800

    终于能跑通了
    remember 一下

--- index.js
+++ index.js
@@ -4,29 +4,31 @@ let sql = require('mssql');
 let sd = require('silly-datetime');
 
 const url = 'https://www.sina.com.cn/';
-const config = ""
-// request(url,function (err,res,body) {
-//     if (!err && res.statusCode == 200) {
-//         $ = cheerio.load(body);
-//         let info = [];
-//         $(".news_top  a").each(function (i, item) {
-            let time = sd.format(new Date(), 'YYYY-MM-DD HH:mm:ss');
-//             let text = $(this).text();
-            let text = 1234;
-            sql.connect(config).then(function () {
+const config = "";
+
+
+let time = sd.format(new Date(), 'YYYY-MM-DD HH:mm:ss');
+let text = 1234;
+//
+sql.connect(config).then(function () {
+
+    request(url, function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+            $ = cheerio.load(body);
+            let info = [];
+            $(".news_top  a").each(function (i, item) {
+                let text = $(this).text();
                 // Query
-                let sqlrun = "insert into t_info (iid,cdt,content) values (null,'" + time + "','" + text + "')";
+                let sqlrun = "insert into t_info (cdt,content) values ('" + time + "','" + text + "')";
                 console.log(sqlrun);
                 new sql.Request().query(sqlrun).then(function (recordset) {
                     console.log(recordset);
                 }).catch(function (err) {
                     console.log(err);
                 });
-            }).catch(function (err) {
-                console.log(err);
             });
-//
-//         });
-//     }
-// });
-
+        }
+    });
+}).catch(function (err) {
+    console.log(err);
+});
